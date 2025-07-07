4fd95e46c63265d79bd34f86f11e3ce4f5b11e44
EugeneIm <immer277@gmail.com>
2025-07-06 22:03:43 -0700

    start of portfolio

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./lib/Header";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <body>
+        <Header />
+      </body>
+    </>
+  );
+}
+
+export default App;
